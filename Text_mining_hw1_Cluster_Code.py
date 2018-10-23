dd41088a0f972f5fa17171fa94ef68fd16cf1a17
pgsecola <42471037+pgsecola@users.noreply.github.com>
2018-10-23 11:30:47 -0400

    Update Text_mining_hw1_Cluster_Code.py

--- Text_mining_hw1_Cluster_Code.py
+++ Text_mining_hw1_Cluster_Code.py
@@ -15,15 +15,13 @@ import sklearn as sl
 import pandas as pd
 import gensim as gs
 
-
-
 os.chdir(r"C:\\Users\\pseco\\Documents\\GitHub\\Text-Mining\\") 
 #os.chdir("C:\\Users\\gflemin\\Documents\\GitHub\\Text-Mining\\")
 
 df = pd.read_csv(r'US_Strat_sample.csv') # what's up with the r's?
 
-df['description'] = df['description'].str.replace('\d+', '')
 
+df['description'] = df['description'].str.replace('\d+', '')
 variety = list(df['variety'])
 doc = []
 descriptions = df['description']
@@ -54,8 +52,9 @@ print(token_term_vec[0:10])
 
 # add words that might be relevant stop words
 stopwords = nltk.corpus.stopwords.words('english')
-morewords = ['drink', 'drinks', 'wine', 'wines', 'note', 'notes', 'flavor', 'flavors',
-             'wine', 'wines', 'grape', 'grapes']
+morewords = ['drink', 'show', 'touch', 'mouth', 'feel', 'nose', 'drinks', 'wine', 'wines', 
+             'wine', 'wines', 'grape', 'grapes', 'note', 'notes', 'aroma', 'aromas', 'palate', 'finish', 'taste', 
+             'tastes', 'show', 'flavour', 'flavor', 'flavors', 'flavours', 'fruit', '%', '.', '-', '-.','(',')']
 stopwords.extend(morewords)
 
 #print(stopwords)
@@ -158,13 +157,14 @@ for i in doc:
     allwords_tokenized = tokenize_only(i)
     totalvocab_tokenized.extend(allwords_tokenized)
 #Define Vectorizer Parameters
-
+import sklearn as sk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.spatial.distance import cdist
 from sklearn.cluster import KMeans
 from sklearn.externals import joblib
 import numpy as np
+import matplotlib.pyplot as plt
 
 #computes similarity
 tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000, min_df=0.2, stop_words=stopwords, use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1,3))
@@ -180,7 +180,7 @@ dist_matrix_one = 1-cosine_similarity(part_one)
 terms= tfidf_vectorizer.get_feature_names()
 
 #Cluster Analysis
-num_clusters = 50
+num_clusters = 10
 
 km = KMeans(n_clusters=num_clusters)
 
@@ -189,6 +189,27 @@ km.fit(tfidf_matrix)
 clusters=km.labels_.tolist()
 
 clusters = km.labels_.tolist()
+
+#plot use elbow method to determine cluster
+
+mms=sk.preprocessing.MaxAbsScaler()
+mms.fit(tfidf_matrix)
+transformed_matrix = mms.transform(tfidf_matrix)
+
+sum_of_squared_dist = []
+Kl=range(1,50)
+
+for k in Kl:
+    km = KMeans(n_clusters=k)
+    km = km.fit(transformed_matrix)
+    
+    sum_of_squared_dist.append(km.inertia_)
+
+plt.plot(Kl, sum_of_squared_dist, 'bx-')
+plt.xlabel('K')
+plt.ylabel('sum_of_squared_dist')
+plt.title('optimal k')
+plt.show()
 
 #Visualize Cluster
 reviews = {'Wine':variety, 'Reviews':doc,'cluster':clusters}
@@ -201,8 +222,6 @@ print(cluster_frame[0:10])
 #subset matrix
                  
 #create data frame that has the result of the MDS plus the cluster numbers and titles
-
-import os  # for os.path.basename
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -229,34 +248,41 @@ df_2 = pd.DataFrame(dict(x=xs, y=ys, clusters=clusters, title=variety))
 groups = df_2.groupby('clusters')
 
 # set up plot
-fig, ax = plt.subplots(figsize=(17, 9)) # set size
+fig, ax = plt.subplots(figsize=(20, 20)) # set size
 ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
 
 #iterate through groups to layer the plot
 #note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
 for name, group in groups:
     ax.plot(group.x, group.y, marker='o', linestyle='', ms=12, 
-            label=clusters, 
+            label=clusters,  
             mec='none')
+    ax.set_xlim([-.9,.9])
+    ax.set_ylim([-.9,.9])
     ax.set_aspect('auto')
     ax.tick_params(\
         axis= 'x',          # changes apply to the x-axis
         which='both',      # both major and minor ticks are affected
-        bottom=False,      # ticks along the bottom edge are off
+        bottom=True,      # ticks along the bottom edge are off
         top=False,         # ticks along the top edge are off
-        labelbottom=False)
+        labelbottom=True)
     ax.tick_params(\
         axis= 'y',         # changes apply to the y-axis
         which='both',      # both major and minor ticks are affected
-        left=False,      # ticks along the bottom edge are off
+        left=True,      # ticks along the bottom edge are on
         top=False,         # ticks along the top edge are off
-        labelleft=False)
+        labelleft=True
+        )
 
 for i in range(len(df_2)):
-    ax.text(df_2.loc[i]['x'], df_2.loc[i]['y'], df_2.loc[i]['title'], size=8)  
+    ax.text(df_2.loc[i]['x'], df_2.loc[i]['y'], df_2.loc[i]['clusters'], size=24)  
 
-    
     
 plt.show() #show the plot
 
-    
+centroids=km.cluster_centers_
+
+centroids_x = centroids[:,0]
+centroids_y=centroids[:,1]
+
+plt.scatter(centroids_x, centroids_y, color='black', label=clusters)
